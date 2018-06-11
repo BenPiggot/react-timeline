8e5b8f2d8acf3fde20ff991a0a694e2913febd44
ben <wbpiggot@gmail.com>
2018-06-10 22:07:45 -0700

    adding CSS transition

--- src/Panel.tsx
+++ src/Panel.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { CSSTransitionGroup } from 'react-transition-group';
 import{ DataObject } from '../types';
 import './Panel.scss';
 
@@ -6,12 +7,33 @@ interface PanelProps {
   selectedDataObject: DataObject 
 }
 
-class Panel extends React.Component<PanelProps, {}> {
+interface PanelState {
+  show: boolean
+  entered: boolean
+}
+
+class Panel extends React.Component<PanelProps, PanelState> {
+  constructor(props: PanelProps) {
+    super(props);
+
+    this.state = {
+      show: false,
+      entered: false
+    }
+  }
+
   render() {
     return (
       <div className="panel">
-        <h1>{this.props.selectedDataObject.year}</h1>
-        <img src={this.props.selectedDataObject.image} />
+        <CSSTransitionGroup
+          transitionName="panel-content"
+          transitionEnterTimeout={300}
+          transitionLeaveTimeout={300}
+          style={{ display: 'flex' }}
+        > 
+            <h1>{this.props.selectedDataObject.year}</h1>
+            <img src={this.props.selectedDataObject.image} key={this.props.selectedDataObject.image}/>
+        </CSSTransitionGroup>
       </div>
     )
   }
